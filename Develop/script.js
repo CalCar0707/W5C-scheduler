f2c7dec5e517da5e4c9bd241aa7e3bd58be3ece7
Call Carl <callienicole0419@gmail.com>
2022-12-07 12:41:07 -0600

    added jquery link to html and some in js

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,10 @@ $('#currentDay').text(today.format('dddd MMMM, Do'));
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(() => {
+    $('.btn saveBtn').on('click', )
+
+})
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
