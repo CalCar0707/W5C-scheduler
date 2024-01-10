aa47c30d4235e82db862c202ee5fc4824e7f50e8
CalCarl <callienicole0419@gmail.com>
2024-01-10 16:38:18 -0600

    fixed iteration for saved notes to show from local storage, removed unused variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,22 +6,8 @@ $currentDay.text(todaysDate.format('dddd, MMMM D, YYYY'));
 
 var userInput = $('#user-input');
 
-//variables for diff hrs
-const hrNine = $('#hour-9').text();
-const hrTen = $('#hour-10').text();
-const hrEleven = $('#hour-11').text();
-const hrTwelve = $('#hour-12').text();
-const hrOne = $('#hour-1').text();
-const hrTwo = $('#hour-2').text();
-const hrThree = $('#hour-3').text();
-const hrFour = $('#hour-4').text();
-const hrFive = $('#hour-5').text();
-
 
 $(document).ready( function () {
-
-
-  readNotesFromStorage();
 
   //save note to local storage
   $('.saveBtn').on('click', function (event) {
@@ -43,6 +29,7 @@ function timeColor () {
 
       let hour = parseInt($(this).attr('id'));
       
+      //assigns past, present, or fututre color code to time blocks
       if(currentTime === hour) {
         $(this).addClass('present');
       } else if(hour > currentTime) {
@@ -51,52 +38,26 @@ function timeColor () {
         $(this).addClass('past');
       }
    })
-
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
     }
 
       function readNotesFromStorage(){
     
-        // $('#user-input').each( function () {
-
-          // let time = $(this).parent().attr('id');
-          // let savedNote = localStorage.getItem(time);
-          // console.log(time);
-          // console.log(savedNote);
-
-          // if(!savedNote){
-          //   return;
-          // }
-        // $(this).siblings('.description').textContent = savedNote;
-
-        // })
-        //ONLY 9 HR BLOCK IS PERSISTING
-        $('#user-input').each( function () {
-        let test = 'where is this  going to show up?';
+      //retirieves saved notes from local storage and displays each
+        $('.description').each( function () {
+    
         let time = $(this).parent().attr('id');
         let savedNote = localStorage.getItem(time);
-        //shows up in 9 hr block
-        //userInput.val(savedNote);
-         $(this).val(savedNote);
-       
-       
-       //does not show output
-        //$(this).textContent = savedNote;
+        return $(this).val(savedNote);
+          
         })
 
   };
 
-
-
-    readNotesFromStorage();
-    timeColor();
+   readNotesFromStorage();
+   timeColor();
     
 
   });
   
 
-
+ 
